7c678a622b343e0c2c613d82e25b0e63fed1ad17
Armand Mégrot <armand.megrot@gmail.com>
2019-10-06 17:42:29 +0100

    Adding batman sound on konami code

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import GlobalStyle from '../styles/global'
-import SEO from './SEO'
-import 'typeface-cantata-one'
-import 'typeface-open-sans'
+import React from 'react';
+import KonamiCode from 'konami-code';
+import GlobalStyle from '../styles/global';
+import SEO from './SEO';
+import ImBatman from '../sounds/im-batman.mp3';
+import 'typeface-cantata-one';
+import 'typeface-open-sans';
+
+new KonamiCode().listen(() => {
+  console.log('ma bite');
+  new Audio(ImBatman).play();
+});
 
 const Layout = () => (
   <>
     <SEO />
     <GlobalStyle />
   </>
-)
+);
 
-export default Layout
+export default Layout;
