0238e04abae806a10de9477490559c43464d0171
Armand Mégrot <armand.megrot@gmail.com>
2022-11-20 22:03:39 +0100

    Change emoji to icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,47 @@
 <script lang="ts">
-  import { GithubIcon } from 'svelte-feather-icons';
-  import RocketScienceHero from '$lib/assets/images/RocketScienceHero.jpg';
-  import ExposeHero from '../lib/assets/images/ExposeHero.jpg';
-  import MastodonPersoHero from '$lib/assets/images/MastodonPersoHero.jpg';
-  import PortfolioHero from '$lib/assets/images/PortfolioHero.jpg';
+  import { BookIcon, FilterIcon, GithubIcon, MessageCircleIcon, RssIcon } from "svelte-feather-icons";
+  import RocketScienceHero from "$lib/assets/images/RocketScienceHero.jpg";
+  import ExposeHero from "../lib/assets/images/ExposeHero.jpg";
+  import MastodonPersoHero from "$lib/assets/images/MastodonPersoHero.jpg";
+  import PortfolioHero from "$lib/assets/images/PortfolioHero.jpg";
+  import type { SvelteComponent } from "svelte";
 
   interface Project {
     heroImg: string;
     title: string;
-    emoji?: string;
+    icon?: SvelteComponent;
     description: string;
   }
 
   const projects: Project[] = [
     {
       heroImg: RocketScienceHero,
-      title: 'Rocket Science',
-      emoji: '🚀',
-      description: 'Experimentation project using a sport API. The goal is to experiment with the Dynamic Island & LiveActivities API on iOS.',
-      link: 'https://github.com/armandmgt/rocket-science'
+      title: "Rocket Science",
+      icon: FilterIcon,
+      description: "Experimentation project using a sport API. The goal is to experiment with the Dynamic Island & LiveActivities API on iOS.",
+      link: "https://github.com/armandmgt/rocket-science"
     },
     {
       heroImg: ExposeHero,
-      title: 'Expose',
-      emoji: '🚇',
-      description: 'Implementing a reverse proxy to expose local web applications. The local proxy also exposes a web dashboard to inspect and replay requests.',
-      link: 'https://github.com/armandmgt/expose'
+      title: "Expose",
+      icon: RssIcon,
+      description: "Implementing a reverse proxy to expose local web applications. The local proxy also exposes a web dashboard to inspect and replay requests.",
+      link: "https://github.com/armandmgt/expose"
     },
     {
       heroImg: MastodonPersoHero,
-      title: 'Self-hosted mastodon',
-      emoji: '💬',
-      description: 'I am self-hosting a mastodon personal instance. The purpose was to avoid lags and also to understand how it works.',
-      link: 'https://mastodon.armandmgt.fr'
+      title: "Self-hosted mastodon",
+      icon: MessageCircleIcon,
+      description: "I am self-hosting a mastodon personal instance. The purpose was to avoid lags and also to understand how it works.",
+      link: "https://mastodon.armandmgt.fr"
     },
     {
       heroImg: PortfolioHero,
-      title: 'This portfolio',
-      emoji: '📓',
-      description: 'Showing off the few side-projects I am happy about. I am not a good designer so please be kind :)',
-      link: 'https://github.com/armandmgt/armandmgt.github.io'
-    },
+      title: "This portfolio",
+      icon: BookIcon,
+      description: "Showing off the few side-projects I am happy about. I am not a good designer so please be kind :)",
+      link: "https://github.com/armandmgt/armandmgt.github.io"
+    }
   ];
 
   let descShown = false;
@@ -48,32 +49,38 @@
 </script>
 
 <nav>
-    <button
-            class="absolute z-10 bottom-10 right-10 bg-black dark:bg-white w-24 h-24 hover:scale-125 transition-all rounded-full text-xl font-light text-transparent hover:text-white dark:hover:text-black"
-            on:click={toggleDesc}
-    >
-        {#if descShown}HIDE{:else}SHOW{/if}
-    </button>
+  <button
+    class="absolute z-10 bottom-10 right-10 bg-black dark:bg-white w-24 h-24 hover:scale-125 transition-all rounded-full text-xl font-light text-transparent hover:text-white dark:hover:text-black"
+    on:click={toggleDesc}
+  >
+    {#if descShown}HIDE{:else}SHOW{/if}
+  </button>
 </nav>
 
 <main class="p-4 h-full">
-    <h1 class="mb-10">Things I did</h1>
-    <div class="flex grow overflow-x-auto">
-        <div class="flex">
-            {#each projects as project}
-                <div class="relative w-96 flex flex-col overflow-hidden m-1 last:mr-32">
-                    <h3>{project.title} {project.emoji}</h3>
-                    <img src={project.heroImg} alt={project.title} class="grow object-cover"/>
-                    <p class="absolute border-2 p-2 text-black bg-white dark:text-white dark:bg-black transition-all overflow-hidden" style:bottom={descShown ? '0' : '-100px'}>
-                        {project.description}
-                    </p>
-                </div>
-            {/each}
+  <h1 class="mb-10">Things I did</h1>
+  <div class="flex grow overflow-x-auto">
+    <div class="flex">
+      {#each projects as project}
+        <div class="relative w-96 flex flex-col overflow-hidden m-1 last:mr-32">
+          <h3>
+            {project.title}
+            <svelte:component this={project.icon} class="inline" />
+          </h3>
+          <img src={project.heroImg} alt={project.title} class="grow object-cover" />
+          <p
+            class="absolute border-2 p-2 text-black bg-white dark:text-white dark:bg-black transition-all overflow-hidden"
+            style:bottom={descShown ? '0' : '-100px'}>
+            {project.description}
+          </p>
         </div>
+      {/each}
     </div>
-    <footer>
-        <a href="https://github.com/armandmgt/armandmgt.github.io" class="text-xs">
-            <GithubIcon size="12" class="inline mr-1"/>See on Github
-        </a>
-    </footer>
+  </div>
+  <footer>
+    <a href="https://github.com/armandmgt/armandmgt.github.io" class="text-xs">
+      <GithubIcon size="12" class="inline mr-1" />
+      See on Github
+    </a>
+  </footer>
 </main>
