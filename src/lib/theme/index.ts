885843731365abbc5d1234f83a995a5236c90ab1
Armand Mégrot <armand.megrot@gmail.com>
2022-11-20 20:05:13 +0100

    Re-implement site using SvelteKit

---
+++ src/lib/theme/index.ts
@@ -0,0 +1,48 @@
+import { writable as localStorageWritable } from 'svelte-local-storage-store';
+import { get, writable } from 'svelte/store';
+import { browser } from '$app/environment';
+
+const defaultTheme = 'dark';
+
+export const theme = localStorageWritable('theme', defaultTheme);
+
+const detectSystemPreference = (e: MediaQueryListEvent) => applyTheme(e.matches ? 'dark' : 'light');
+
+/**
+ * Applies system preference theme and registers a listener for changes
+ */
+export const initTheme = async (): Promise<void> => {
+  if (!browser) return;
+
+  window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', detectSystemPreference);
+
+  if ('theme' in localStorage && theme) {
+    try {
+      const pref = get(theme);
+      if (pref) {
+        applyTheme(pref as string);
+      }
+    } catch (err) {
+      console.error(err);
+      localStorage.removeItem('theme');
+    }
+  } else {
+    applyTheme('system');
+  }
+  initComplete.set(true);
+};
+
+export const initComplete = writable(false);
+
+export const applyTheme = (newTheme: string): void => {
+  if (!browser) return;
+
+  const className = newTheme === 'system' ?
+    (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light') :
+    newTheme;
+
+  document?.documentElement?.classList.remove('light', 'dark');
+  document?.documentElement?.classList.add(className);
+  theme.set(newTheme);
+};
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import '../app.css';
+  import { browser } from '$app/environment';
+  import { initTheme, initComplete as themeInitComplete } from '$lib/theme';
+  import ThemeSelector from './ThemeSelector.svelte';
+
+  if (browser)
+      import('tw-elements');
+  initTheme();
+</script>
+
+{#if $themeInitComplete}
+    <div class="relative h-screen bg-white dark:bg-black dark:text-white">
+        <ThemeSelector/>
+
+        <slot></slot>
+    </div>
+{/if}
