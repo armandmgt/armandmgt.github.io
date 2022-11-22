ba12bdd9abf79fe7247bff389eed5dc3f86b5e97
Armand Mégrot <armand.megrot@gmail.com>
2022-11-22 22:49:52 +0100

    Add safelist for dynamically set classes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/tw-elements/dist/js/**/*.js'],
   darkMode: 'class',
+  safelist: ['text-white', 'text-black', 'border-white', 'border-black'],
   theme: {
     extend: {},
   },
