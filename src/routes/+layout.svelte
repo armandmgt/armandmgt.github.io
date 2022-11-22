7643fbc7282949ab62069e05ed75208e2ff524ae
Armand Mégrot <armand.megrot@gmail.com>
2022-11-22 22:16:59 +0100

    Add site title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -77,6 +77,7 @@
 </script>
 
 <svelte:head>
+  <title>{siteName}</title>
   <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
