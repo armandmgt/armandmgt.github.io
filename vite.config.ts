65e18aabd00e68379483c168deecfcfb795d41f3
Armand Mégrot <armand.megrot@gmail.com>
2024-10-09 00:09:59 +0200

    NoBuild

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { imagetools } from 'vite-imagetools';
-import type { UserConfig } from 'vite';
-
-const config: UserConfig = {
-	plugins: [sveltekit(), imagetools()]
-};
-
-export default config;
